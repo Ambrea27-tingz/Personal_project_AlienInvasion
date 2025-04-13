889593c17a7e2e8d235e3adaf58daf15665ca0cf
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-04-13 13:52:14 -0400

    created a ship class & set up to draw to screen

--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ class Settings:
         #frames per second
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_w = 40
+        self.ship_h = 60

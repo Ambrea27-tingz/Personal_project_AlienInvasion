bfd40a5a7712fa5a0af96e91094c27bf5ea42352
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-04-13 22:32:34 -0400

    P2 - finished fleet reacting to boundries and shifting down.

--- settings.py
+++ settings.py
@@ -24,6 +24,6 @@ class Settings:
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_w  = 80
         self.alien_h = 40
-        self.fleet_speed = 4  #originally 5
+        self.fleet_speed = 2  #originally 5
         self.fleet_direction = 1 
         self.fleet_drop_speed = 40
